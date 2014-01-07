1b1297b4b09a69d02d48024997b4c7254d332bed
root <root@ip-10-134-10-57.ap-southeast-1.compute.internal>
2014-01-07 18:42:54 +0000

    Fixed

--- app/models/Entity/DailyStatsEntity.php
+++ app/models/Entity/DailyStatsEntity.php
@@ -6,7 +6,7 @@ class DailyStatsEntity extends BaseEntity {
 
 	public function getStatsForCampaign($networkCode, $campaignCode) {
 		return $this->getTable()
-			->select('SUM(clicks_cnt) AS clicks_cnt, SUM(downloads_cnt) AS downloads_cnt')
+			->select('SUM(clicks_cnt) AS clicks_cnt, SUM(downloads_cnt) AS downloads_cnt, MAX(date) AS last_update_date')
 			->where('network_code', $networkCode)
 			->where('campaign_code', $campaignCode)
 			->fetch();
--- app/modules/ApiModule/presenters/CampaignPresenter.php
+++ app/modules/ApiModule/presenters/CampaignPresenter.php
@@ -12,14 +12,24 @@ use \Nette\Application\UI\Form;
  */
 class CampaignPresenter extends BasePresenter {
 
-	/** @autowire @var \Models\DailyStats */
+	/** @autowire @var \HQ\Model\Entity\DailyStatsEntity */
 	protected $dailyStatsModel;
 
-	public function actionDefault($networkCode, $campaignCode) {
-
+	public function actionDefault() {
+		$networkCode = $this->getHttpRequest()->getQuery('network_code');
+		$campaignCode = $this->getHttpRequest()->getQuery('campaign_code');
+		
 		$dailyStats = $this->dailyStatsModel->getStatsForCampaign($networkCode, $campaignCode);
-
-		$this->prepareAndSendValidResponse($dailyStats);
+		
+		$ret = array(
+			'clicks_cnt' => $dailyStats['clicks_cnt'],
+			'downloads_cnt' => $dailyStats['downloads_cnt'],
+			'network_code' => $networkCode,
+			'campaign_code' => $campaignCode,
+			'last_update_date' => $dailyStats['last_update_date']->format('Y-m-d H:i:s')
+		);
+		
+		$this->prepareAndSendValidResponse($ret);
 	}
 
 }
