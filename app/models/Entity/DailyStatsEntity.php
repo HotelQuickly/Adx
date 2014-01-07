3845641e8cf6c7d1b84546d0e9bbbfff4fc7ce2c
Michal Juhas <michal.juhas@hotelquickly.com>
2014-01-07 18:57:49 +0100

    API for campaign info

--- app/models/Entity/DailyStatsEntity.php
+++ app/models/Entity/DailyStatsEntity.php
@@ -4,5 +4,12 @@ namespace HQ\Model\Entity;
 
 class DailyStatsEntity extends BaseEntity {
 
+	public function getStatsForCampaign($networkCode, $campaignCode) {
+		return $this->getTable()
+			->select('SUM(clicks_cnt) AS clicks_cnt, SUM(downloads_cnt) AS downloads_cnt')
+			->where('network_code', $networkCode)
+			->where('campaign_code', $campaignCode)
+			->fetch();
+	}
 	
 }
